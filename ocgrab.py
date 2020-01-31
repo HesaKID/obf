9a28e10fbb13cc1e82624881a7b3163b152d716b
HesaKID <48668839+HesaKID@users.noreply.github.com>
2020-01-31 21:45:18 +0700

    Add files via upload

--- ocgrab.py
+++ ocgrab.py
@@ -2,7 +2,8 @@ import requests as r
 import os
 import sys
 from platform import system
-
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
 
 
 if system() == 'Linux':
@@ -13,14 +14,14 @@ if system() == 'Windows':
 def grab(url):
     if "//" not in url:
         url = "%s%s" % ('http://', url)
-        url = url.strip()
-        head = {'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0'}
-        cek = r.get(url + '/admin/index.php', headers = head)
-        if "common/login" in cek.text:
-            print(url + " [OK]")
-            open('ocg_result.txt', 'a').write(url + '\n')
-        else:
-            print(url + " [BAD]")
+    url = url.strip()
+    head = {'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0'}
+    cek = r.get(url + '/admin/index.php', headers = head)
+    if "common/login" in cek.text:
+        print(url + " [OK]")
+        open('ocg_result.txt', 'a').write(url + '\n')
+    else:
+        print(url + " [BAD]")
 
 def main():
     listsite = open(sys.argv[1], 'r').readlines()
